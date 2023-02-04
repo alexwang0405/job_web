e5ad0c180ef8bc456edfffa6cfc852f45e107ff2
Alex Wang <alexwang0405@gmail.com>
2023-02-04 22:31:43 +0800

    feat: 增加收藏工作API

--- jobs/views.py
+++ jobs/views.py
@@ -3,6 +3,8 @@ from .serializers import JobSerializer, PlatformSerializer, UserSerializer
 from .models import Job, Platform
 from django.contrib.auth.models import User
 from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
 # Create your views here.
@@ -31,3 +33,43 @@ class UserViewSet(viewsets.ModelViewSet):
     permission_classes = [
         permissions.IsAdminUser
     ]
+
+
+class FavorAPI(APIView):
+
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        
+        user = User.objects.filter(id=request.user.id).first()
+        favor_job = user.job_set.all()
+
+        return Response({
+            'status': 'OK',
+            'data': [{
+                'job_id': row.id,
+                'job_name': row.job_name, 
+                'job_link': row.job_link,
+                } for row in favor_job],
+        })
+
+    def post(self, request):
+
+        user = User.objects.filter(id=request.user.id).first()
+        job = Job.objects.filter(id=request.data.get('job_id')).first()
+
+        job.users.add(user)
+        job.save()
+
+        return Response({'status': 'OK'})
+
+    def delete(self, request):
+
+        user = User.objects.filter(id=request.user.id).first()
+        job = Job.objects.filter(id=request.data.get('job_id')).first()
+
+        job.users.remove(user)
+        job.save()
+
+        return Response({'status': 'OK'})
+
--- myweb/urls.py
+++ myweb/urls.py
@@ -17,4 +17,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('logout_token/', LogoutAPIView.as_view(), name='logout_token'),
+    path('favor/', views.FavorAPI.as_view(), name='favor')
 ]
